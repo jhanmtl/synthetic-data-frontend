de3aec5def6a840cb328def11be4889733de3abe
jhanmtl <jing.han@mail.mcgill.ca>
2020-07-19 14:32:59 -0400

    Updates

---
+++ precache-manifest.4436d1271f3474a8ff5e7bbb29c47b9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6597bb3693e836ac0ec609522c0a39a5",
+    "url": "/synthetic-data-frontend/index.html"
+  },
+  {
+    "revision": "f3e867a22deb10720b3a",
+    "url": "/synthetic-data-frontend/static/css/main.10ab5291.chunk.css"
+  },
+  {
+    "revision": "812a8f18f17b573da61f",
+    "url": "/synthetic-data-frontend/static/js/2.e37aaece.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/synthetic-data-frontend/static/js/2.e37aaece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3e867a22deb10720b3a",
+    "url": "/synthetic-data-frontend/static/js/main.1b214bdf.chunk.js"
+  },
+  {
+    "revision": "6aa3a8312daa02632544",
+    "url": "/synthetic-data-frontend/static/js/runtime-main.2804bf3f.js"
+  }
+]);
